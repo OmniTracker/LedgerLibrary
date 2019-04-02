3202bcfca7ef2b4d015e9d058b4dac099e120556
Ronald Baker <omnitalos@Ronalds-MacBook-Pro.local>
2019-04-02 09:28:35 -0400

    Added new functions

--- contracts/BookLedger.sol
+++ contracts/BookLedger.sol
@@ -5,24 +5,25 @@ import "./Tokens/ERC721.sol";
 /**
  * This contract inherits from ERC721
 
-Every User/address/entity has a library of books. Calling this contract is equivalent to creating their own library.
+Every User/address/entity has a library of books. Calling this contract is
+equivalent to creating their own library.
 
  */
 contract BookLedger is ERC721 {
   string public constant contractName = 'BookLedger'; // For testing.
 
   event ledgerCreated(address book);
-  event entityInitialized(address owner);  
+  event entityInitialized(address owner);
   event bookRemoved();
   event bookAdded(string publisher, string author, string name );
   event bookRemoved(string publisher, string author, string name );
 
   struct Book {
     uint256 bookID;
-    uint256 bookPointer;    
+    uint256 bookPointer;
     uint256 timeOfOrigin;
-    uint256 timeOfRental;
     bool availability;
+    string bookPossession;
     uint256 genre;
     string country;
     string publisher;
@@ -30,17 +31,18 @@ contract BookLedger is ERC721 {
     string name;
   }
 
-
   address[] public _entity;
-  
+
   mapping(address => mapping(uint256 => Book)) _books; // index books by entity address and bookID
+  mapping(uint256 => Book) _lostBooksList;
   mapping(address => uint256[]) _booksList;
+
+  // Commit books for user
+  mapping(address => mapping(address => mapping(uint256 => bool))) internal _committed;
 
   address public _minter;
   uint256 public _startBalance;
   uint256 public _rentalInterval;
-
-
   constructor(
     address minter,
     uint256 startBalance
@@ -48,21 +50,18 @@ contract BookLedger is ERC721 {
       _minter = minter;
       _startBalance = startBalance;
   }
-
   modifier onlyMinter() {
     require(msg.sender == _minter, "msg.sender is not _minter.");
     _;
   }
-
   modifier exists(uint256 bookID) {
       require(_exists(bookID), "ERC721 token exists for book with specified ID does not exist.");
     _;
   }
-
-  
   /**
    * Mint new book to library
-     May want when creating a book to make a hash of the concat of some of the properties of the book for sharing
+     May want when creating a book to make a hash of the concat of some of the
+     properties of the book for sharing
    */
    function newBook ( address owner,
 		      uint256 bookID,
@@ -77,7 +76,6 @@ contract BookLedger is ERC721 {
        bookID: bookID,
        bookPointer: 0,
        timeOfOrigin: now,
-       timeOfRental: 0,
        availability: true,
        genre: genre,
        country: country,
@@ -91,59 +89,37 @@ contract BookLedger is ERC721 {
      _books[owner][bookID] = book;
      /* Mint new book to the library */
      _mint(owner, bookID);
-
-     emit bookAdded(_books[owner][bookID].publisher, _books[owner][bookID].author, _books[owner][bookID].name );
+     emit bookAdded(_books[owner][bookID].publisher, _books[owner][bookID].author, _books[owner][bookID].name);
      return bookID;
    }
-
-
    /**
     * Get origin date of book being placed in library.
     */
-
    function originDateOfBook ( address owner, uint256 bookID )
       external exists(bookID) view returns(uint256)
     {
-	return _books[owner][bookID].timeOfOrigin;
+	     return _books[owner][bookID].timeOfOrigin;
     }
-
    /**
     * Get number of books in Library
     */
     function numberOfBookInLibraray ( address owner ) public view returns(uint256) {
-      return _booksList[owner].length;
+        return _booksList[owner].length;
     }
-
    /**
     * Get the availability of book from library.
     */
     function isBookAvailable( address owner, uint256 bookID ) public exists(bookID) view returns(bool)
     {
-      return _books[owner][bookID].availability;
+        return _books[owner][bookID].availability;
     }
-
-  /**
-   * Commit to removing book from the library ledger
-   */
-   function commitBookRemoval ( uint256 bookID ) exists(bookID) public {
-
-   }
-
   /**
    * Remove book from the library
    * @param bookID The unique ID of the book in the library.
    */
    function removeBook( address owner, uint256 bookID ) public exists(bookID) returns(bool) {
-       //require( isBookAvailable( owner, bookID ) );
      Book memory book = _books[owner][bookID];
-     // Commit to taking the book out the Library
-     //commitBookRemoval(bookID);
-     // Set the book as unavailable
-       //book.availability = false;
-     // Set the time of rental
-     //book.timeOfRental = now;
      emit bookRemoved( book.publisher, book.author, book.name );
-
      // remove book from book list by swapping with last element in list
      // and update swapped book's pointer in book mapping
      uint256 rowToDelete = _books[owner][bookID].bookPointer;
@@ -152,40 +128,74 @@ contract BookLedger is ERC721 {
      _books[owner][keyToMove].bookPointer = rowToDelete;
      _booksList[owner].length--;
      _burn(owner, bookID);
-     
      return true;
    }
 
  /**
   * Place book back into the library
   */
-  function returnBook( uint256 bookID ) exists(bookID) public {
+  function returnBook( address owner, uint256 bookID ) exists(bookID) public
+  {
+    // Commit book back to being sent to library
+
 
   }
-
   /**
-   * Renew library book subscription
-   */
-   function renewBookRental( uint256 bookID ) exists(bookID) public {
+    * Signal that the book has physically been returned to the library
+    */
+   function archiveBook( address owner, uint256 bookID ) exists(bookID) public
+   {
+     // Only the library should be able to place the book back into the archive
 
    }
+  /**
+   *  Request book from the library
+   */
+   function requestBook ( address owner, uint256 bookID ) exists(bookID) public
+   {
+     // You should not be able to request a book that you are the owner of
+     requires(msg.sender != owner);
+
+   }
+  /**
+    * Accept that the book is currntly in your possesion.
+    */
+    function acceptBook ( address owner, uint256 bookID ) exists(bookID) public
+    {
+        // Check the status of if the book is being sent or not.
+
+    }
+
+    /**
+      * Trade Book
+      */
+    function tradeBook ( address owner, uint256 bookID ) exists(bookID) public
+    {
+
+    }
+  /**
+    *
+    */
+    function lostBook ( address owner, uint256 bookID ) exists(bookID) public
+    {
+      // Can not lose the same book twice, so will not allow the book to
+      // be re added to the lost book list.
+       requires(_lostBooksList[bookID] != _books[owner][bookID]);
+       // Only the
+
+       Book memory book = _books[owner][bookID];
+        _lostBooksList[bookID] = book;
+        removeBook( owner, bookID );
+    }
 
   /**
-   * Get date of book removal from library
-   */
-   function getDateOfBookRemoval( uint256 bookID )
-      external exists(bookID) view returns(uint256)
-    {
-
-    }
-
-   /**
-    * Get date when book needs to be submitted back to library
+    * Commit to removing book from the library ledger
     */
-    function returnTimeOfBook ( uint256 bookID )
-      external exists(bookID) view returns(uint256)
+    function commitBook ( address owner, uint256 bookID ) exists(bookID) public
     {
+      // Only the library should be able to remove the book from this place.
+      requires(owner == msg.sender);
+      Book memory book = _books[owner][bookID];
 
     }
-
 }
